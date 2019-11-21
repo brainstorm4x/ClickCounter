b0f5d81d85a0a180804c25b06fb7a66104de3456
Barry Richards <youremail@domain.com>
2019-11-21 13:30:41 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  const increment = () => {
+    setCount(count + 1)
+  }
+
+  return(
+    <div>
+    <h3>Click Counter</h3>
+    <button onClick={increment}>Clicked {count} times</button>
     </div>
   );
 }
+
+
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       count: 0
+//     }
+//   this.increment = this.increment.bind(this);
+//   }
+//
+//   increment(){
+//     this.setState({
+//       count: this.state.count + 1
+//     })
+//   }
+//
+//   render(){
+//     return(
+//       <div>
+//         <h4>This is my Counter App!</h4>
+//         <button onClick={this.increment}>Clicked {this.state.count} times</button>
+//       </div>
+//     );
+//   }
+// }
+
 
 export default App;
